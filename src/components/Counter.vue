a743de2619aadf4b71450316d5025d4a52fee91b
Charygulyyev <99390211+Charygulyyev@users.noreply.github.com>
2022-02-19 04:24:41 +0300

    commit1

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="decr">-</button>
+  <div>{{c}}</div>
+  <button @click="incr">+</button>
+</template>
+
+<script>
+export default {
+name: 'CounteR',
+props: {
+    count: Number,
+},
+data(){
+    return {
+        c: this.count
+    }
+},
+methods:{
+    decr(){
+        this.c -= 1
+    },
+    incr(){
+        this.c += 1
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Counters.vue
@@ -0,0 +1,43 @@
+<template>
+  <input type="number" v-model="new_counter">
+  <button @click="add_counter">add</button>
+  <br />
+  <template v-for="(count, ind) in counters" :key="ind">
+    <Counter :count="count"/>
+    <button @click="delete_counter(ind)">delete</button>
+    <hr v-if="ind+1 < get_length"/>
+  </template>
+</template>
+
+<script>
+import Counter from './Counter.vue'
+export default {
+name: 'GreeT',
+components: {
+  Counter
+},
+data(){
+  return{
+    new_counter: 0,
+    counters: [],
+  }
+},
+computed:{
+  get_length(){
+    return this.counters.length
+  }
+},
+methods:{
+  add_counter(){
+    this.counters = [...this.counters,this.new_counter]
+    this.new_counter = 0
+  },
+  delete_counter(par){
+    this.counters = this.counters.filter( (e,i) => i !== par )
+  }
+}
+}
+</script>
+
+<style>
+</style>
