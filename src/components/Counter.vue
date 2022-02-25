77484eb196b80af4d2ace0153b7746eefe333430
Charygulyyev <99390211+Charygulyyev@users.noreply.github.com>
2022-02-26 01:24:02 +0300

    counters modified

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,29 +1,24 @@
 <template>
-  <button @click="decr">-</button>
-  <div>{{c}}</div>
-  <button @click="incr">+</button>
+    <div>
+        <form @submit.prevent="">
+            <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+        </form>
+        <button @click="$emit('decr', count.id)">-</button>
+        <div>{{ count.value }}</div>
+        <button @click="$emit('incr', count.id)">+</button>
+        <button @click="$emit('delete', count.id)">del</button>
+    </div>
 </template>
 
 <script>
 export default {
-name: 'CounteR',
-props: {
-    count: Number,
-},
-data(){
-    return {
-        c: this.count
-    }
-},
-methods:{
-    decr(){
-        this.c -= 1
-    },
-    incr(){
-        this.c += 1
-    }
-}
-}
+  name: "CounteR",
+  props: {
+    count: Object,
+    modelValue: String
+  },
+  emits: ["delete", "incr", "decr"],
+};
 </script>
 
 <style>
--- src/components/Counters.vue
+++ src/components/Counters.vue
@@ -1,39 +1,89 @@
 <template>
-  <input type="number" v-model="new_counter">
-  <button @click="add_counter">add</button>
-  <br />
-  <template v-for="(count, ind) in counters" :key="ind">
-    <Counter :count="count"/>
-    <button @click="delete_counter(ind)">delete</button>
+  <form @submit.prevent="add_counter">
+    <input type="number" v-model.lazy="new_counter.value">
+    <button @click.prevent="add_counter">add</button>
+  </form>
+  <hr />
+  <template v-for="(count, ind) in counters" :key="count.id">
+    <Counter 
+      :count="count"
+      @incr="incr_counter"
+      @decr="decr_counter"
+      @delete="delete_counter"
+      v-model="name"
+      />
     <hr v-if="ind+1 < get_length"/>
   </template>
 </template>
 
 <script>
 import Counter from './Counter.vue'
+
 export default {
-name: 'GreeT',
+
+name: 'CounterS',
 components: {
   Counter
 },
+
 data(){
   return{
-    new_counter: 0,
+    new_counter: {
+      id: '',
+      value: 0,
+    },
     counters: [],
+    name: "",
   }
 },
+
 computed:{
   get_length(){
     return this.counters.length
   }
 },
+
 methods:{
-  add_counter(){
-    this.counters = [...this.counters,this.new_counter]
-    this.new_counter = 0
+  incr_counter(id){
+    this.counters.map(counter => {
+      if (counter.id === id){
+        counter.value += 1
+      }
+    })
   },
+
+  decr_counter(id){
+    this.counters.map(counter => {
+      if (counter.id === id){
+        counter.value -= 1
+      }
+    })
+  },
+
+  uuid(){
+    let timeStamp = new Date;
+    let id = 
+      timeStamp.getFullYear() + '/' + 
+      timeStamp.getMonth() + '/' + 
+      timeStamp.getDate() + '/' +
+      timeStamp.getMilliseconds() + ':' +
+      timeStamp.getSeconds() + ':' +
+      timeStamp.getMinutes() + ':' +
+      timeStamp.getHours();
+    this.new_counter.id = id
+  },
+
+  add_counter(){
+    this.uuid();
+    this.counters = [this.new_counter, ...this.counters];
+    this.new_counter = {
+      id: '',
+      value: 0
+    }
+  },
+
   delete_counter(par){
-    this.counters = this.counters.filter( (e,i) => i !== par )
+    this.counters = this.counters.filter( (e) => e.id !== par )
   }
 }
 }
